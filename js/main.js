301673c3944db78461301ef0871f97a2c124f36c
roni1 <ronifraimovich@gmail.com>
2016-02-05 14:06:35 -0500

    Remove redundancies

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
-$(function(){
-  var browser = navigator.appName;
-  var java = navigator.javaEnabled();
-  var platform = navigator.platform;
-  alert("Your browser is " + browser + ". Your platform is " + platform +". Your Java is " + (java ? "enabled" : "disabled") + ".");
-});
+var browser = navigator.appName;
+var java = navigator.javaEnabled();
+var platform = navigator.platform;
+alert("Your browser is " + browser + ". Your platform is " + platform +". Your Java is " + (java ? "enabled" : "disabled") + ".");
