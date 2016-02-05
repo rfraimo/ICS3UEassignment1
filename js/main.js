17ed0ed71e88124bdfed8d180d5767f7f8d65575
roni1 <ronifraimovich@gmail.com>
2016-02-05 14:05:54 -0500

    Add project so far to main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 $(function(){
-  // jQuery goes here
+  var browser = navigator.appName;
+  var java = navigator.javaEnabled();
+  var platform = navigator.platform;
+  alert("Your browser is " + browser + ". Your platform is " + platform +". Your Java is " + (java ? "enabled" : "disabled") + ".");
 });
